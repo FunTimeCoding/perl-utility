ea469e43bd8311013a4dd212e2585c88591794d1
Alexander Reitzel <funtimecoding@gmail.com>
2018-08-06 18:20:12 +0200

    add config reading

--- module/perl_utility/perl_utility.pm
+++ module/perl_utility/perl_utility.pm
@@ -5,6 +5,12 @@ package perl_utility::perl_utility;
 
 sub main
 {
+    my $configuration_file = $ENV{"HOME"} . '/.perl-skeleton.pl';
+    open CONFIGURATION, "$configuration_file" or die "Configuration missing: $configuration_file\n";
+    my $configuration = join "", <CONFIGURATION>;
+    close CONFIGURATION;
+    eval $configuration;
+    die "Configuration error: $@\n" if $@;
     print "Hello friend.\n";
 
     return(0);
