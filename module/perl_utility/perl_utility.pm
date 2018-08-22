93f434cbb2a62f7304542624390d0a377f9ddcae
Alexander Reitzel <funtimecoding@gmail.com>
2018-08-22 14:25:20 +0200

    declare global token so it can be modified inside the eval

--- module/perl_utility/perl_utility.pm
+++ module/perl_utility/perl_utility.pm
@@ -8,10 +8,14 @@ sub main
     my $configuration_file = $ENV{"HOME"} . '/.perl-utility.pl';
     open CONFIGURATION, "$configuration_file" or die "Configuration missing: $configuration_file\n";
     my $configuration = join "", <CONFIGURATION>;
+
+    our $TOKEN = "unset";
     close CONFIGURATION;
     eval $configuration;
     die "Configuration error: $@\n" if $@;
+
     print "Hello friend.\n";
+    print "Token: $TOKEN\n";
 
     return(0);
 }
